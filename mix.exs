23c5760b39adce10b65c7644e5a4710a38ce7c3e
Jurfest <jurfest.apps@gmail.com>
2022-11-21 09:16:39 -0300

    test: install mock and use it to verify trimmed

--- mix.exs
+++ mix.exs
@@ -24,7 +24,8 @@ defmodule PluralsightTweet.MixProject do
     [
       {:credo, "~> 1.6"},
       {:quantum, "~> 3.5"},
-      {:extwitter, "~> 0.14.0"}
+      {:extwitter, "~> 0.14.0"},
+      {:mock, "~> 0.3.7"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
--- test/pluralsight_tweet/file_reader_test.exs
+++ test/pluralsight_tweet/file_reader_test.exs
@@ -1,12 +1,10 @@
 defmodule PluralsightTweet.FileReaderTest do
   use ExUnit.Case
   import PluralsightTweet.FileReader
+  import Mock
 
   test "Passing a file should return a string" do
-    str =
-      get_strings_to_tweet(
-        Path.join("#{:code.priv_dir(:pluralsight_tweet)}", "sample.txt")
-      )
+    str = get_strings_to_tweet(Path.join("#{:code.priv_dir(:pluralsight_tweet)}", "sample.txt"))
 
     assert str != nil
   end
@@ -22,10 +20,16 @@ defmodule PluralsightTweet.FileReaderTest do
 
   test "Empty string should return an empty string" do
     str =
-      get_strings_to_tweet(
-        Path.join("#{:code.priv_dir(:pluralsight_tweet)}", "/test/empty.txt")
-      )
+      get_strings_to_tweet(Path.join("#{:code.priv_dir(:pluralsight_tweet)}", "/test/empty.txt"))
 
     assert str == ""
+  end
+
+  test "The string should be trimmed" do
+    with_mock File, [read!: fn _ -> " ABC " end] do
+      str = get_strings_to_tweet("doesn't exist.txt")
+
+      assert str == "ABC"
+    end
   end
 end
